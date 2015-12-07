c270b19cee171c16d49eedb49c695d3fc41a2331
Ochi <ochi@arcor.de>
2015-12-07 15:25:49 +0100

    Updated Dockerfile and build script

--- docker_build.sh
+++ docker_build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+set -e
+
 PROJECT="$1"
 ARCHIVE=${PROJECT}_latest.tar.gz
 
 cd /tmp
-curl -O http://0x00.cc/pc2bim/${ARCHIVE}
+curl -O https://0x00.cc/pc2bim/${ARCHIVE}
 tar -C / -xf ${ARCHIVE}
 rm ${ARCHIVE}
 ldconfig
